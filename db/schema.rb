6bfb0607c9fdc9df30d5a83748cd0ef79ff423f4
johan febvre <j0h4n.LEWAGONE@gmail.com>
2017-02-22 22:52:40 +0100

    Added rows to user model, seeded, removed a few broken links still need a lot of work in terms of style

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222111903) do
+ActiveRecord::Schema.define(version: 20170222183837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,9 @@ ActiveRecord::Schema.define(version: 20170222111903) do
     t.string   "weapons"
     t.boolean  "asset_status",           default: true
     t.string   "picture"
+    t.text     "profile_description"
+    t.string   "number_of_kills"
+    t.string   "xp_level"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
