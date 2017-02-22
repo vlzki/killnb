3fb7aefaefd32d384fe4d47dcb36a125ce14d4bd
johan febvre <j0h4n.LEWAGONE@gmail.com>
2017-02-22 13:12:26 +0100

    Attachinary - done + Add a picture btn on booking form and sign_up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
+	mount Attachinary::Engine => "/attachinary"
+	devise_for :users
+	root to: 'pages#home'
 
-  devise_for :users
-  root to: 'pages#home'
-
-  resources :assets, only: [:index, :show] do
-    resources :jobs, only: [:index, :show, :new, :create, :destroy]
-  end
+	resources :assets, only: [:index, :show] do
+		resources :jobs, only: [:index, :show, :new, :create, :destroy]
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
