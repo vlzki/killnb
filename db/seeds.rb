29c58dc8f5eb3ddfc2c7f9004987a4516f168381
vlzki <vlzki.dev@gmail.com>
2017-02-23 12:55:44 +0100

    change in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Assets.destroy.all
-urls.destroy.all
 
 assets = [
   {
