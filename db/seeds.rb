4db044947247166763eee2dcb1dd7dcd9242ffea
vlzki <vlzki.dev@gmail.com>
2017-02-23 12:42:28 +0100

    minor change of seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+assets.destroy.all
+urls.destroy.all
 
 assets = [
   {
